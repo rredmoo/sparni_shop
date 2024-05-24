7316e1211b64055b427282538f6447c762976839
Davids-Adamovics <davidsadamovicss@gmail.com>
2024-05-24 11:45:46 +0300

    initial commit

---
+++ src/main/java/lv/venta/SparniShopApplication.java
@@ -0,0 +1,13 @@
+package lv.venta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparniShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SparniShopApplication.class, args);
+	}
+
+}
