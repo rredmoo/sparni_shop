cc97d83a7150d9145035b4fdc69a8b5742061839
ossitiss <s23valtoska@venta.lv>
2024-05-31 10:23:01 +0300

    oskars update

--- src/main/java/lv/venta/model/Pasakumi.java
+++ src/main/java/lv/venta/model/Pasakumi.java
@@ -6,6 +6,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -20,9 +21,29 @@ import lombok.ToString;
 
 public class Pasakumi {
 	
-	public Pasakumi(String title, int creditpoints) {
-		setTitle(title);
+	@Id
+	@Column (name = "IDP")
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Setter(value = AccessLevel.NONE)
+	private int idp;
+	
+	@Column (name = "NOSAUKUMS")
+	private String nosaukums;
+	
+	@Column (name = "APRAKSTS")
+	private String apraksts;
+	
+	@Column (name = "SAKUMA_DATUMS")
+	private int sakuma_datums;
+	
+	@Column (name = "BEIGU_DATUMS")
+	private int beigu_datums;
+	
+	@Column (name = "LOKACIJA")
+	private int lokacija;
+	
+	//public Pasakumi(String title) {
 		
-	}
+	//}
 
 }
