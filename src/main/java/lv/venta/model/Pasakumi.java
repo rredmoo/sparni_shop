2b796938662253d7c4ffb0729e460f84e753f067
ossitiss <s23valtoska@venta.lv>
2024-05-31 09:54:37 +0300

    oskars update test

---
+++ src/main/java/lv/venta/model/Pasakumi.java
@@ -0,0 +1,28 @@
+package lv.venta.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+@Table(name = "PASAKUMI_TABLE")
+@Entity
+
+public class Pasakumi {
+	
+	public Pasakumi(String title, int creditpoints) {
+		setTitle(title);
+		
+	}
+
+}
