0362f8ed522c05779ebb6203798df1c63e3d0176
Kārlis Bērziņš <151729535+BAARDA01@users.noreply.github.com>
2024-06-17 23:22:12 +0300

    Added ServiceRepo : Kontakti , Biedribas darbojas , ParMums

--- src/main/java/lv/venta/service/IKontaktiCRUDService.java
+++ src/main/java/lv/venta/service/IKontaktiCRUDService.java
@@ -1,5 +1,19 @@
 package lv.venta.service;
 
+import java.util.ArrayList;
+
+import lv.venta.model.Kontakti;
+
 public interface IKontaktiCRUDService {
 
+	void create(Kontakti kontakti);
+
+    Kontakti retrieveById(int id) throws Exception;
+
+    ArrayList<Kontakti> retrieveAll() throws Exception;
+
+    void updateById(int id, Kontakti kontakti) throws Exception;
+
+    void deleteById(int id) throws Exception;
+	
 }
--- src/main/java/lv/venta/service/IMainPageBiedribasDarbojasCRUDService.java
+++ src/main/java/lv/venta/service/IMainPageBiedribasDarbojasCRUDService.java
@@ -1,5 +1,21 @@
 package lv.venta.service;
 
+import java.util.ArrayList;
+
+import lv.venta.model.MainPage_BiedribaDarbojas;
+
 public interface IMainPageBiedribasDarbojasCRUDService {
 
+
+	void create(MainPage_BiedribaDarbojas mainPageBiedribaDarbojas);
+
+    MainPage_BiedribaDarbojas retrieveById(int id) throws Exception;
+
+    ArrayList<MainPage_BiedribaDarbojas> retrieveAll() throws Exception;
+
+    void updateById(int id, MainPage_BiedribaDarbojas mainPageBiedribaDarbojas) throws Exception;
+
+    void deleteById(int id) throws Exception;
+	
+	
 }
