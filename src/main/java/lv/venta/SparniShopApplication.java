6be256c191e493d72fb2c1e31f33e94efcfa65c4
GrecAlek <s23grecalek@venta.lv>
2024-06-22 21:22:20 +0300

    aaa

--- src/main/java/lv/venta/SparniShopApplication.java
+++ src/main/java/lv/venta/SparniShopApplication.java
@@ -39,6 +39,7 @@ public class SparniShopApplication {
 				Atlaide a2 = new Atlaide(50, LocalDateTime.of(2024,05,10,15,30), LocalDateTime.of(2024,05,15,15,31), true);
 				atlaideRepo.save(a2);
 
+			
 			}
 		};
 
