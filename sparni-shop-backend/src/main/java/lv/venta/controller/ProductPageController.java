87b303941eec3a38d934dbaecc9636b12b4eccf8
Kārlis Bērziņš <151729535+BAARDA01@users.noreply.github.com>
2024-06-26 23:00:08 +0300

    Added ExceptionHandler

--- sparni-shop-backend/src/main/java/lv/venta/controller/ProductPageController.java
+++ sparni-shop-backend/src/main/java/lv/venta/controller/ProductPageController.java
@@ -1,18 +1,22 @@
 package lv.venta.controller;
 
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import lv.venta.model.Atlaide;
+
 import lv.venta.model.Veikals_prece;
-import lv.venta.service.IAtlaideService;
 import lv.venta.service.IPreceCRUDService;
 
 @CrossOrigin(origins = "http://localhost:3000")
@@ -73,5 +77,10 @@ public void deletePreceById(@PathVariable int id) {
         
     }
 }
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNoSuchElementFoundException(NoSuchElementException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+            .body("Item not found");
+    }
 
 }
