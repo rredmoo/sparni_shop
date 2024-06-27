68bfdbb2784f2641ff5d5b0dd08324c21cd8a230
Davids-Adamovics <davidsadamovicss@gmail.com>
2024-06-27 22:40:17 +0300

    Fiexed jaunumi page and added design

--- sparni-shop-frontend/src/components/jaunumuPage/pasakums.jsx
+++ sparni-shop-frontend/src/components/jaunumuPage/pasakums.jsx
@@ -10,10 +10,10 @@ function Pasakums({ numPasakumi }) {
     JaunumiServiceConfig.getAllPasakumi()
       .then((response) => {
         if (Array.isArray(response.data)) {
-          const sortedPasakumi = response.data.sort((a, b) => b.idPasakumi - a.idPasakumi);
-          setPasakumi(sortedPasakumi);
-          const limitedPasakumi = response.data.slice(0, numPasakumi);
-          setPasakumi(limitedPasakumi); // Set state with limitedPasakumi
+          const sortedPasakumi = response.data.sort(
+            (a, b) => b.idPasakumi - a.idPasakumi
+          );
+          setPasakumi(sortedPasakumi.slice(0, numPasakumi));
         } else {
           console.error("Expected an array but got:", response.data);
           setPasakumi([]);
@@ -27,56 +27,70 @@ function Pasakums({ numPasakumi }) {
 
   return (
     <>
-      <div className="pasakumu-list">
+      <div className="pasakumi-container">
         {pasakumi.length > 0 ? (
           <>
-            {/* lielais pasakums */}
-            {pasakumi.length > 0 && (
-              <div className={`pasakums-card latest-pasakums`} key={pasakumi[0].idPasakumi}>
-                <img
-                  className="pasakums-card-img"
-                  src={pasakumi[0].bildesUrl}
-                  alt={pasakumi[0].nosaukums}
-                />
-                <div className="pasakums-card-details">
-                  <h3>{pasakumi[0].nosaukums}</h3>
-                  <p>{pasakumi[0].apraksts}</p>
-                </div>
+            <div className="pasakumi-top">
+              <div className="pasakumi-left">
+                {/* Lielais pasakums */}
+                {pasakumi.length > 0 && (
+                  <div
+                    className={`pasakums-card latest-pasakums`}
+                    key={pasakumi[0].idPasakumi}
+                  >
+                    <img
+                      className="pasakums-card-img-large"
+                      src={pasakumi[0].bildesUrl}
+                      alt={pasakumi[0].nosaukums}
+                    />
+                    <div className="pasakums-card-details">
+                      <h3>{pasakumi[0].nosaukums}</h3>
+                      <p>{pasakumi[0].apraksts}</p>
+                    </div>
+                  </div>
+                )}
               </div>
-            )}
 
-            {/* mazie pasakumi labaja puse*/}
-            {pasakumi.slice(1, 3).map((pasakums) => (
-              <div className={`pasakums-card latest-small-pasakums`} key={pasakums.idPasakumi}>
-                <img
-                  className="pasakums-card-img"
-                  src={pasakums.bildesUrl}
-                  alt={pasakums.nosaukums}
-                />
-                <div className="pasakums-card-details">
-                  <h3>{pasakums.nosaukums}</h3>
-                  <p>{pasakums.apraksts}</p>
-                </div>
+              <div className="pasakumi-right">
+                {/* Mazie pasakumi labajā pusē */}
+                {pasakumi.slice(1, 3).map((pasakums) => (
+                  <div
+                    className={`pasakums-card latest-small-pasakums`}
+                    key={pasakums.idPasakumi}
+                  >
+                    <img
+                      className="pasakums-card-img"
+                      src={pasakums.bildesUrl}
+                      alt={pasakums.nosaukums}
+                    />
+                    <div className="pasakums-card-details">
+                      <h3>{pasakums.nosaukums}</h3>
+                      <p>{pasakums.apraksts}</p>
+                    </div>
+                  </div>
+                ))}
               </div>
-            ))}
+            </div>
 
-            {/* Atdalosa linija */}
-            <hr className="separator-line" />
-
-            {/* Parada visus pasakumus */}
-            {pasakumi.slice(3).map((pasakums) => (
-              <div className="pasakums-card" key={pasakums.idPasakumi}>
-                <img
-                  className="pasakums-card-img"
-                  src={pasakums.bildesUrl}
-                  alt={pasakums.nosaukums}
-                />
-                <div className="pasakums-card-details">
-                  <h3>{pasakums.nosaukums}</h3>
-                  <p>{pasakums.apraksts}</p>
+            <div className="pasakumi-under">
+              {/* Mazie pasakumi zem */}
+              {pasakumi.slice(3).map((pasakums) => (
+                <div
+                  className="pasakums-card rest-of-pasakumi"
+                  key={pasakums.idPasakumi}
+                >
+                  <img
+                    className="pasakums-card-img"
+                    src={pasakums.bildesUrl}
+                    alt={pasakums.nosaukums}
+                  />
+                  <div className="pasakums-card-details">
+                    <h3>{pasakums.nosaukums}</h3>
+                    <p>{pasakums.apraksts}</p>
+                  </div>
                 </div>
-              </div>
-            ))}
+              ))}
+            </div>
           </>
         ) : (
           <div>No Pasakumi available</div>
