bde0e2038367d03fa7f9fdee2f0d8a712d4166b1
Davids-Adamovics <davidsadamovicss@gmail.com>
2024-10-23 21:12:45 +0300

    Added epastiNoKlientaRepo filtrations

--- sparni-shop-backend/src/main/java/lv/venta/repo/EpastiNoKlientaRepo.java
+++ sparni-shop-backend/src/main/java/lv/venta/repo/EpastiNoKlientaRepo.java
@@ -2,11 +2,24 @@ package lv.venta.repo;
 
 import lv.venta.model.EpastiNoKlienta;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 
 
 
 public interface EpastiNoKlientaRepo extends JpaRepository<EpastiNoKlienta, Long> {
 
+    // basic filtrations by id, name, topic, email
     ArrayList<EpastiNoKlienta> findByIdenk(int idenk);
+    ArrayList<EpastiNoKlienta> findByUserNameIgnoreCase(String userName);
+    ArrayList<EpastiNoKlienta> findByUserEmailIgnoreCase(String userEmail);
+    ArrayList<EpastiNoKlienta> findByTopicIgnoreCase(String topic);
+
+    //filter by date
+    ArrayList<EpastiNoKlienta> findByReceivedAtAfter(LocalDateTime date);
+    ArrayList<EpastiNoKlienta> findByReceivedAtBefore(LocalDateTime date);
+    ArrayList<EpastiNoKlienta> findByReceivedAtBetween(LocalDateTime startDate, LocalDateTime endDate);
+
+
 }
