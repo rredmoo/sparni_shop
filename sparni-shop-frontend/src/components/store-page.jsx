266e0813e0fa175d25bf77973d9b2e10774ccf27
Dāvids Adamovičs <152193570+Davids-Adamovics@users.noreply.github.com>
2024-11-24 09:59:56 +0000

    Added banner

--- sparni-shop-frontend/src/components/store-page.jsx
+++ sparni-shop-frontend/src/components/store-page.jsx
@@ -1,14 +1,17 @@
 import React, { useState } from "react";
 import Header from "./common/Header";
 import Product from "./veikalsPage/product";
-import { useTranslation } from "react-i18next"; 
-import StoreBanner from "../static/img/storeBanner.png";
+import { useTranslation } from "react-i18next";
+import leftImage from "../static/img/leftImage.png";
+import rightImageTop from "../static/img/rightImageTop.png";
+import rightImageBottomLeft from "../static/img/rightImageBottomLeft.png";
+import rightImageBottomRight from "../static/img/rightImageBottomRight.png";
 import "../static/css/Store.css";
 
 function StorePage() {
   const [error, setError] = useState(null);
   const [sortOrder, setSortOrder] = useState(null);
-  const { t } = useTranslation(); 
+  const { t } = useTranslation();
 
   const handleSortOrder = (order) => {
     setSortOrder(order);
@@ -18,13 +21,29 @@ function StorePage() {
     <>
       <Header />
       <div className="main-page-cover">
-      <img src={StoreBanner} alt="Store Page Banner" className="storeBannerImage" />
-      <h1>{t('storeHeader')}</h1>
-      <div>
-        <button className="storeSortingButton" onClick={() => handleSortOrder('asc')}>{t('asc')}</button>
-        <button onClick={() => handleSortOrder('desc')}>{t('dsc')}</button>
-      </div>
-      <Product sortOrder={sortOrder} />
+
+        <div class="container">
+          <div class="gallery">
+            <figure class="gallery__item gallery__item--1">
+              <img src={leftImage} alt="Gallery image 1" class="gallery__img" />
+            </figure>
+            <figure class="gallery__item gallery__item--2">
+              <img src={rightImageTop} alt="Gallery image 2" class="gallery__img" />
+            </figure>
+            <figure class="gallery__item gallery__item--3">
+              <img src={rightImageBottomLeft} alt="Gallery image 3" class="gallery__img" />
+            </figure>
+            <figure class="gallery__item gallery__item--4">
+              <img src={rightImageBottomRight} alt="Gallery image 4" class="gallery__img" />
+            </figure>
+          </div>
+        </div>
+        <h1>{t('storeHeader')}</h1>\
+        <div>
+          <button className="storeSortingButton" onClick={() => handleSortOrder('asc')}>{t('asc')}</button>
+          <button onClick={() => handleSortOrder('desc')}>{t('dsc')}</button>
+        </div>
+        <Product sortOrder={sortOrder} />
       </div>
     </>
   );
