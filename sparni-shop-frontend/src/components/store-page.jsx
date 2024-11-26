0416befdbea668004e76489dc9cee2ebfd9e8dcd
Davids-Adamovics <davidsadamovicss@gmail.com>
2024-11-26 02:07:33 +0200

    FIXED: banner image not being aligned

--- sparni-shop-frontend/src/components/store-page.jsx
+++ sparni-shop-frontend/src/components/store-page.jsx
@@ -21,29 +21,47 @@ function StorePage() {
     <>
       <Header />
       <div className="main-page-cover">
-
         <div class="container">
           <div class="gallery">
             <figure class="gallery__item gallery__item--1">
               <img src={leftImage} alt="Gallery image 1" class="gallery__img" />
             </figure>
             <figure class="gallery__item gallery__item--2">
-              <img src={rightImageTop} alt="Gallery image 2" class="gallery__img" />
+              <img
+                src={rightImageTop}
+                alt="Gallery image 2"
+                class="gallery__img"
+              />
             </figure>
             <figure class="gallery__item gallery__item--3">
-              <img src={rightImageBottomLeft} alt="Gallery image 3" class="gallery__img" />
+              <img
+                src={rightImageBottomLeft}
+                alt="Gallery image 3"
+                class="gallery__img"
+              />
             </figure>
             <figure class="gallery__item gallery__item--4">
-              <img src={rightImageBottomRight} alt="Gallery image 4" class="gallery__img" />
+              <img
+                src={rightImageBottomRight}
+                alt="Gallery image 4"
+                class="gallery__img"
+              />
             </figure>
           </div>
         </div>
-        <h1>{t('storeHeader')}</h1>\
+
         <div>
-          <button className="storeSortingButton" onClick={() => handleSortOrder('asc')}>{t('asc')}</button>
-          <button onClick={() => handleSortOrder('desc')}>{t('dsc')}</button>
+          <button
+            className="storeSortingButton"
+            onClick={() => handleSortOrder("asc")}
+          >
+            {t("asc")}
+          </button>
+          <button onClick={() => handleSortOrder("desc")}>{t("dsc")}</button>
         </div>
-        <Product sortOrder={sortOrder} />
+        <div className="storeItems">
+          <Product sortOrder={sortOrder} />
+        </div>
       </div>
     </>
   );
